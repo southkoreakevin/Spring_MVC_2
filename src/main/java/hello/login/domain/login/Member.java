6eea84234b14fccdb9d4cd65b7acac60f2f2c8a7
kevin <dongohkang@gangdong-oui-MacBookPro.local>
2026-01-17 00:30:52 +0900

    1/17

---
+++ src/main/java/hello/login/domain/login/Member.java
@@ -0,0 +1,16 @@
+package hello.login.domain.login;
+
+import javax.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Data
+public class Member {
+    private Long id;
+
+    @NotEmpty
+    private String loginId;
+    @NotEmpty
+    private String name;
+    @NotEmpty
+    private String password;
+}
